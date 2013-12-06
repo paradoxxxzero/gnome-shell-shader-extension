682f4d09d486adb35f242d84c59b6ba6857abede
Florian Mounier <florian.mounier@kozea.fr>
2013-12-06 12:17:49 +0100

    Gnome 3.10 support ah yeah!

--- example.shader.glsl
+++ example.shader.glsl
@@ -1,22 +1,22 @@
+uniform int height;
 uniform sampler2D tex;
-uniform float height;
-uniform float width;
+uniform int width;
 
-float r = 1.;
-float sn = .2;
 
-void main()
-{
-  vec2 resolution = vec2(width, height);
-  vec4 col = vec4(0., 0., 0., 0.);
-  float d = 1.;
-  for (float i = -r; i <= r; i++) {
-    for (float j = -r; j <= r; j++) {
-      vec2 sh = vec2(i, j) / resolution.xy;
-      if(i == 0. && j == 0.) d = .9;
-      else d = sn / (i*i + j*j);
-      col += texture2D(tex, cogl_tex_coord_in[0].xy + sh) * d;
+float r = 2.;
+float sn = .25;
+vec2 resolution = 1 / vec2(width, height);
+
+void main(void) {
+  vec2 col = cogl_tex_coord_in[0].xy;
+  vec4 sum = vec4(0);
+
+  for (float i = -r; i < r; i++) {
+    if (i != 0) {
+      float d = sn / (i * i);
+      sum += (texture2D(tex, col + vec2(i, 0) * resolution) + texture2D(tex, col + vec2(0, i) * resolution)) * d;
     }
   }
-  cogl_color_out = vec4(col.xyz, texture2D(tex, cogl_tex_coord_in[0].xy).w);
+  cogl_color_out = (texture2D(tex, col) + sum);
+  cogl_color_out.a *= .25;
 }
